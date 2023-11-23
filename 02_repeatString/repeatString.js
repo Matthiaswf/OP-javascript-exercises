88554303abd54567febaf4af3b744933520a94d3
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2023-11-23 15:03:53 +0900

    Exercise 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(inputText, inputNumber) {
+    let outputText = "";
+    if ( inputNumber < 0){
+        return "ERROR";
+    }
+    for (let i = 0; i < inputNumber; i++) {
+        outputText += inputText;   
+      }
+      return outputText;    
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
