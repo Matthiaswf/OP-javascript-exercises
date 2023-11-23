2e00dd82404388df9006e7dd5eeb2ceaa7b3eb66
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2023-11-23 16:04:24 +0900

    First attempt

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,12 @@
 /*
+
+Assignment = Return the input string reversed
+
 Variable we need:
 
 1.	inputString
-2.	newString 
+2.	reverseString 
+3.  newArray
 
 Method:
 
@@ -10,13 +14,24 @@ Method:
 2. Turn inputString into an Array
 3. Get the array length
 4. Loop splicing from the end to the start into a new array
-5. Print the new Array into newString
-6.Set inputString = newString
+5. Print the new Array into reverseString
+6.Set inputString = reverseString
 */
 
+let newArray = [""];
+let inputString = "";
+let reverString = "";
 
-const reverseString = function() {
+const reverseString = function(inputString) {
+   let splitString = inputString.split("");
+   let splitStringLength = inputString.length();
 
+   for (let i = 0; i < splitStringLength; i++) {
+   let lastElement = splitString.pop(-1);
+   newArray.push(lastElement); 
+  }
+  reverseString = newArray.join("");
+  return reverString;
 };
 
 // Do not edit below this line
