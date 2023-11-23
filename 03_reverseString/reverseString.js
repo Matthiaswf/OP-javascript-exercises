908e1cd829faf3aa5f1c1a873b742f67ba617d96
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2023-11-23 15:32:21 +0900

    Exercise 3 Pseudo code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,3 +1,20 @@
+/*
+Variable we need:
+
+1.	inputString
+2.	newString 
+
+Method:
+
+1. Take inputString
+2. Turn inputString into an Array
+3. Get the array length
+4. Loop splicing from the end to the start into a new array
+5. Print the new Array into newString
+6.Set inputString = newString
+*/
+
+
 const reverseString = function() {
 
 };
