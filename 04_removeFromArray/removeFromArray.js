20f77fece5a5cbaf6b276b7659e38c7a1364906a
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2023-11-25 22:10:21 +0900

    exr3 test 1 passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
 let removeFromArray = function(arrayInput, elementInput) {
-    let index = arrayInput.indexOf(elementInput);
-    return arrayInput.splice(index, 1)
+    const index = arrayInput.indexOf(elementInput);
+    arrayInput.splice(index, 1);
+    console.log('finaleValue', arrayInput);
+    return arrayInput;
 };
 
 // Do not edit below this line
