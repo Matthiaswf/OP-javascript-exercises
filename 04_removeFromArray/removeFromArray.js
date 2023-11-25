c1fc04030557858d87267d29a6245d9a0c8bc8fa
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2023-11-25 22:50:00 +0900

    test 3 aber schellen vom SR

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,27 @@
 let removeFromArray = function(arrayInput, elementInput, elementInputTwo) {
-    const index = arrayInput.indexOf(elementInput);
-    arrayInput.splice(index, 1);
-    if( elementInputTwo != undefined){
-        const index2 = arrayInput.indexOf(elementInputTwo);
-        arrayInput.splice(index2, 1);
+    if (elementInput != undefined) {
+        let index = arrayInput.indexOf(elementInput);
+            if (index === -1) {
+
+            }
+            else {
+                arrayInput.splice(index, 1);
+            }
+    }
+    console.log('finaleValue', arrayInput);
+    if (elementInputTwo != undefined) {
+        let index2 = arrayInput.indexOf(elementInputTwo);
+            if ( index2 === -1){
+
+            }
+            else {
+                arrayInput.splice(index2, 1);
+                console.log(index2);
+            }
     }
     console.log('finaleValue', arrayInput);
     return arrayInput;
 };
-//removeFromArray([1, 2, 3, 4], 3, 2);
+//removeFromArray([1, 2, 3, 4], 3, 6);
 // Do not edit below this line
 module.exports = removeFromArray;
