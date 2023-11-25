e188b24705abc751d9affc8e3e55bacbb03a1438
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2023-11-25 23:01:01 +0900

    "So is nicht schlecht" .Senior

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,16 @@
 let removeFromArray = function(arrayInput, elementInput, elementInputTwo) {
     if (elementInput != undefined) {
-        let index = arrayInput.indexOf(elementInput);
-        if (index != -1) {
-            arrayInput.splice(index, 1);
-        }
+        const index = arrayInput.indexOf(elementInput);
+        if (index !== -1) arrayInput.splice(index, 1);
     }
-    console.log('finaleValue', arrayInput);
+
     if (elementInputTwo != undefined) {
-        let index2 = arrayInput.indexOf(elementInputTwo);
-        if (index2 != -1) {
-            arrayInput.splice(index2, 1)   
-        }       
+        const index2 = arrayInput.indexOf(elementInputTwo);
+        if (index2 !== -1) arrayInput.splice(index2, 1);        
     }
-    console.log('finaleValue', arrayInput);
+
     return arrayInput;
 };
-//removeFromArray([1, 2, 3, 4], 3, 6);
+
 // Do not edit below this line
 module.exports = removeFromArray;
