86969b0922a55d805ea9629568ba1b1912572fb1
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2023-11-25 22:32:46 +0900

    test 2 PASS AMK

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,13 @@
-let removeFromArray = function(arrayInput, elementInput) {
+let removeFromArray = function(arrayInput, elementInput, elementInputTwo) {
     const index = arrayInput.indexOf(elementInput);
     arrayInput.splice(index, 1);
+    if( elementInputTwo != undefined){
+        const index2 = arrayInput.indexOf(elementInputTwo);
+        arrayInput.splice(index2, 1);
+    }
     console.log('finaleValue', arrayInput);
     return arrayInput;
 };
-
+//removeFromArray([1, 2, 3, 4], 3, 2);
 // Do not edit below this line
 module.exports = removeFromArray;
