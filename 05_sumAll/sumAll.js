3dc969faf5c6fabef813bfabc4c80a4b43e7721f
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2023-11-27 21:22:04 +0900

    First try

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,13 @@
+let resultArray = [];
 const sumAll = function(firstInteger, secondInteger) {
-    for (let index = 0; index === 0 | index > firstInteger && index < secondInteger; index++) {
-        resultArray = push(index);
+    for (let index = 0; index === 0 || index > firstInteger && index < secondInteger; index++) {
+        console.log(index);
+        resultArray.push(index);
+        console.log(resultArray);
     }
+    console.log(resultArray);
     return resultArray;
 };
-
+sumAll(1, 4)
 // Do not edit below this line
 module.exports = sumAll;
