d7e766e4a11cbb8b4717f53620a0d31cbb49be46
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2023-11-27 21:16:44 +0900

    First rough code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function(firstInteger, secondInteger) {
+    for (let index = 0; index === 0 | index > firstInteger && index < secondInteger; index++) {
+        resultArray = push(index);
+    }
+    return resultArray;
 };
 
 // Do not edit below this line
