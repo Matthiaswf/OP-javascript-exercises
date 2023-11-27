0cde0084e05a387495e910bf5d081fa4edf668fe
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2023-11-27 20:23:56 +0900

    Test 5 passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,4 @@
-let removeFromArray = function(arrayInput, elementInput, elementInputTwo) {
+let removeFromArray = function(arrayInput, elementInput, elementInputTwo, elementInputThree, elementInputFour) {
     if (elementInput != undefined) {
         const index = arrayInput.indexOf(elementInput);
         if (index !== -1) arrayInput.splice(index, 1);
@@ -7,6 +7,16 @@ let removeFromArray = function(arrayInput, elementInput, elementInputTwo) {
     if (elementInputTwo != undefined) {
         const index2 = arrayInput.indexOf(elementInputTwo);
         if (index2 !== -1) arrayInput.splice(index2, 1);        
+    }
+
+    if (elementInputThree != undefined) {
+        const index3 = arrayInput.indexOf(elementInputThree);
+        if (index3 !== -1) arrayInput.splice(index3, 1);        
+    }
+
+    if (elementInputFour != undefined) {
+        const index4 = arrayInput.indexOf(elementInputFour);
+        if (index4 !== -1) arrayInput.splice(index4, 1);        
     }
 
     return arrayInput;
