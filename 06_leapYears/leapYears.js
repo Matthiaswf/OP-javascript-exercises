3d0c669a997aaaad30d3886b773f89545a777d01
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2023-11-28 14:38:11 +0900

    Basic check if year % = 0 works

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    if ( year % 4 === 0) {
+        console.log('true');   
+    }
+
+    else {
+        console.log('false');
+    }
 
 };
+
+leapYears(1985)
 
 // Do not edit below this line
 module.exports = leapYears;
