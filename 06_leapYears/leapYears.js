ea082e2fb8f5535e828e0bacf9a651dba8f9e347
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2023-11-28 15:31:04 +0900

    Ex6 Test 2 pass

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,16 +1,33 @@
 const leapYears = function(year) {
 
-    if ( year % 4 === 0) {
-        console.log('true');   
+    if ( year % 100 === 0 && year %  400 === 0 ) {
+        //console.log('true');
+
+        return true;
+    }    
+
+    if ( year % 100 === 0 && year % 400 != 0 ) {
+            //console.log('false');
+
+            return false;
     }
 
-    else {
-        console.log('false');
+
+    if ( year % 4 === 0) {
+        //console.log('true');
+
+        return true;
+    } 
+    
+    if ( year % 4 != 0) {
+        //console.log('false');
+
+        return false;
     }
 
 };
 
-leapYears(1985)
+//leapYears(1997)
 
 // Do not edit below this line
 module.exports = leapYears;
