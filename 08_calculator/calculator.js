25fbb18d975da1f8a3cbb1fae3c2a33efcf8b318
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2023-12-05 14:36:50 +0900

    All tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -33,8 +33,13 @@ const power = function(a, b) {
 	return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let f = [];
+  if (n == 0 || n == 1)
+    return 1;
+  if (f[n] > 0)
+    return f[n];
+  return f[n] = factorial(n-1) * n;
 };
 
 // Do not edit below this line
