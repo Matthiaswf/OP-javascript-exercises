f5243613f57caf6f96b006a01c198da2388f20d5
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2023-12-05 15:13:41 +0900

    palindromes complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+const palindromes = function (string) {  
+let compactedString =  string
+.split("")
+.join('')
+.replace(/[^\w\s\']|_/g, '')
+.replace(/\s+/g, "")
+.toLowerCase();
 
+let reversedString = string
+.split('')
+.reverse()
+.join('')
+.replace(/[^\w\s\']|_/g, '')
+.replace(/\s+/g, "")
+.toLowerCase();
+
+    if (compactedString === reversedString){
+
+        return  true;
+    }
+
+    else {
+
+        return false;
+    }
 };
 
 // Do not edit below this line
