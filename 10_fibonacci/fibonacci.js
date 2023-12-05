13c4e121fed630a0082889490f5c87ec3d81b42f
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2023-12-05 15:42:25 +0900

    fibonacci all tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(indexPosition) {
+    if (indexPosition <= 0 ) {
 
-};
+        return 'OOPS';
+    }
+
+    const fibonacciArray = [0, 1];
+     for (let i = 2; i <= indexPosition; i++) {
+        fibonacciArray[i] = fibonacciArray[i - 1] + fibonacciArray[i - 2];
+     }
+
+    return fibonacciArray[indexPosition];
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
