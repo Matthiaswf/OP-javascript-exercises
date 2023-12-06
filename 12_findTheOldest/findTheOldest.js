c6d150ce7012af345e3f628c71beba633afe6a4b
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2023-12-06 14:59:18 +0900

    12 test 1 passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
+ /*const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+*/
+const findTheOldest = function(people) {
+    const oldest = people.sort(function(a, b) {
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
 
+        return lastGuy > nextGuy ? -1 : 1;
+    })
+
+    //console.log(oldest);
+    return oldest[0];
 };
-
+//findTheOldest(people)
 // Do not edit below this line
 module.exports = findTheOldest;
