5271244098417d08348383f35ac95546d4f36d25
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2024-02-08 15:16:09 +0900

    Exercise 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+let reverseString = function(string) {
 
+    reverseString = string.split('').reverse().join('');
+
+    return reverseString;
 };
 
 // Do not edit below this line
