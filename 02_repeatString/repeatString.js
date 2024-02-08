8346f5167806cfadc0aa28f655b1702a5a0661a9
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2024-02-08 15:08:42 +0900

    Exercise 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+let newArray = [''];
+let string = '';
+let number = 0;
 
+let repeatString = function(string, number) {
+    newArray = ['']; 
+
+    if (number < 0 ) {
+        return 'ERROR';
+    }
+
+    for (let i = 0; i < number; i++) {
+       newArray.push(string);
+    }
+    
+    repeatString = newArray.join('')
+    return repeatString;
 };
 
 // Do not edit below this line
